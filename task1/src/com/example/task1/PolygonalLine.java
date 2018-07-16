a3de0c619a758159ec4a6d31c7318df6a348d07f
Boris Tarelkin <batarelkin@chel.rucard.ru>
2018-07-16 12:14:09 +0500

    Initial structure

---
+++ task1/src/com/example/task1/PolygonalLine.java
@@ -0,0 +1,32 @@
+package com.example.task1;
+
+/**
+ * Ломаная линия
+ */
+public class PolygonalLine {
+
+    /**
+     * Устанавливает точки ломаной линии
+     * @param points массив точек, которыми нужно проинициализировать ломаную линию
+     */
+    public void setPoints(Point[] points) {
+        // TODO: реализовать
+    }
+
+    /**
+     * Добавляет точку к ломаной линии
+     * @param point точка, которую нужно добавить к ломаной
+     */
+    public void addPoint(Point point) {
+        // TODO: реализовать
+    }
+
+    /**
+     * Возвращает длину ломаной линии
+     * @return длину ломаной линии
+     */
+    public double getLength() {
+        return 0;
+    }
+
+}
