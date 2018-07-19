22502498cdfaead8fe29507f5ea3c1c482423b73
Boris <tarelkinba@gmail.com>
2018-07-19 14:56:51 +0500

    Задания 6-9

---
+++ task7/src/com/example/task7/Task7Main.java
@@ -0,0 +1,7 @@
+package com.example.task7;
+
+public class Task7Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ task8/src/com/example/task8/Task8Main.java
@@ -0,0 +1,7 @@
+package com.example.task8;
+
+public class Task8Main {
+    public static void main(String[] args) {
+
+    }
+}
